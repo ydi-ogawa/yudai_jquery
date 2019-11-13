aa8d0c7e59bbdb325c0c5bc2f90af5217285e68e
小川雄大 <>
2019-11-13 08:42:35 +0800

    11/13の宿題
    タブの数、スタイル変更

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,19 +41,24 @@ $(function () {
     $(this).siblings().slideToggle();
   });
 
+  /* ハンバーガーメニュー */
   $('#toggle').on('click', function () {
     $(this).toggleClass('on');
   });
+  // onクラスをつけると真ん中のバーは透明に、上下のバーは45度傾いている。
 
   $('.js-scroll').on('click', function () {
     $('body, html').animate({ scrollTop: 0 }, 500);
   });
+  // body,htmlはタグ、cssの設定によっては、うまく認識できない時があるので両方書いてある。
 
+  // モーダル
   $('.js-modal').on('click', function () {
     $('body').addClass('modal-open');
     $('.modal-content').fadeIn('slow');
     $('#modal-bg').fadeIn('slow');
   });
+  // スクロールできないようになっている
 
   $('.js-modal-close').on('click', function () {
     $('body').removeClass('modal-open');
@@ -61,6 +66,46 @@ $(function () {
     $('#modal-bg').fadeOut(1000);
   });
 
+  // // タブメニュー
+  // $(".tab-nav a").on("click", function () {
+  //   if ($(this).hasClass("active")) {
+  //     // 処理をやめさせる
+  //     return false;
+  //   }
+  //   // タブを押すとurlが変わっている
+  //   // アンカーリンクを利用している
+
+  //   // hrefだとurl全体が取得できるが、全ては不要なので、hash使用
+  //   // #以降の値が出力できる
+  //   console.log(this.hash);
+  //   // これはjavascriptの描き方している。ドットで繋げているので
+
+  //   // Javascript
+  //   // .href => URL
+  //   // .textContent => タグの中の文字列
+
+  //   // タブメニューを初期化
+  //   $(".tab-nav a").removeClass("active");
+  //   $(this).addClass("active");
+
+  //   // タブの中身を初期化
+  //   $(".tab-content > div").removeClass("active");
+  //   $(".tab-content > div")
+  //     // filterは検索の意味
+  //     .filter(this.hash)
+  //     // filter('#tab02')だったら、tab02のみしか表示されない。
+  //     // filterの中に条件を入れられる
+  //     .addClass("active");
+
+  //   return false;
+  //   // これはなくても良い
+  //   // この後に処理が書いてあれば書いたほうがいい？
+  // });
+
+
+
+
+  // タブメニュー 練習　タブ増やして
   $(".tab-nav a").on("click", function () {
     if ($(this).hasClass("active")) {
       return false;
@@ -74,10 +119,14 @@ $(function () {
     $(".tab-content > div").removeClass("active");
     $(".tab-content > div")
       .filter(this.hash)
+
       .addClass("active");
 
     return false;
+
   });
+
+
 
   // スライダー
   let slideWidth = $(".slide").outerWidth();
@@ -114,6 +163,8 @@ $(function () {
         left: currentSlide * -slideWidth
       });
   }
+
+
 
 
 
